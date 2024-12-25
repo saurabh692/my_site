1168916ea9222d026539ce1ade79f5f5dfb34fa6
saurabh-eduvance <saurabh@eduvance.in>
2024-12-25 12:51:47 +0530

    first commit

---
+++ blog/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Author,Tag,Post
+# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+  list_filter=("author","tags","date")
+  list_display=("title","date","author")
+  prepopulated_fields={"slug":("title",)}
+admin.site.register(Author)
+admin.site.register(Tag)
+admin.site.register(Post,PostAdmin)
